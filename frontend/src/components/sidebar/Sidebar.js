23f280eaf0462f92571da1b6bdc4b8db19643fbd
infinity1729 <anant.bansal2003@gmail.com>
2023-07-12 23:58:13 +0530

    new commit

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -122,56 +122,6 @@ const Sidebar = (props) => {
               </td>
             </tr>
             <tr>
-              <td>First Name:</td>
-              <td>
-                <input
-                  type="text"
-                  value={firstName}
-                  onChange={(e) => updateFormField('firstName', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Last Name:</td>
-              <td>
-                <input
-                  type="text"
-                  value={lastName}
-                  onChange={(e) => updateFormField('lastName', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Phone Number:</td>
-              <td>
-                <input
-                  type="text"
-                  value={phoneNumber}
-                  onChange={(e) => updateFormField('phoneNumber', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Email ID:</td>
-              <td>
-                <input
-                  type="text"
-                  value={emailAddress}
-                  onChange={(e) => updateFormField('emailAddress', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>LinkedIn URL:</td>
-              <td>
-                <input
-                  type="text"
-                  value={linkedInURL}
-                  onChange={(e) => updateFormField('linkedInURL', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
               <td>Job Title:</td>
               <td>
                 <input
@@ -181,27 +131,69 @@ const Sidebar = (props) => {
                 />
               </td>
             </tr>
-            <tr>
-              <td>Summary:</td>
-              <td>
-                <input
-                  type="text"
-                  value={summary}
-                  onChange={(e) => updateFormField('summary', e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Skills:</td>
-              <td>
-                {skills.map((skill, index) => (
+          </tbody>
+        </table>
+        <div className="block">
+          <h3>Personal Details</h3>
+          <div className="form-group">
+            <label>First Name:</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => updateFormField('firstName', e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => updateFormField('lastName', e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Phone Number:</label>
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => updateFormField('phoneNumber', e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email Id:</label>
+            <input
+              type="text"
+              value={emailAddress}
+              onChange={(e) => updateFormField('emailAddress', e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>LinkeIn Url:</label>
+            <input
+              type="text"
+              value={linkedInURL}
+              onChange={(e) => updateFormField('linkedInURL', e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Summary:</label>
+            <input
+              type="text"
+              value={summary}
+              onChange={(e) => updateFormField('summary', e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="block">
+          <h3>Skills</h3>
+          {skills.map((skill, index) => (
                   <div key={index}>
                     <input
                       type="text"
                       value={skill}
                       onChange={(e) => handleSkillChange(index, e)}
                     />
-                    <button type="button" onClick={() => removeSkill(index)} className="skills-remove-button">
+                    <button type="button" onClick={() => removeSkill(index)} className="remove-button">
                       <FontAwesomeIcon icon={faTrash} />
                     </button>
                   </div>
@@ -209,111 +201,122 @@ const Sidebar = (props) => {
                 <button type="button" onClick={addSkill}>
                   Add Skill
                 </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Education:</td>
-              <td>
-                {education.map((edu, index) => (
-                  <div className="education-container" key={index}>
-                    <label className="education-label">Institution:</label>
-                    <input
-                      type="text"
-                      value={edu.schoolName}
-                      onChange={(e) => handleEducationFieldChange(index, 'schoolName', e.target.value)}
-                      className="education-input"
-                    />
-                    <label className="education-label">Year:</label>
-                    <input
-                      type="text"
-                      value={edu.year}
-                      onChange={(e) => handleEducationFieldChange(index, 'year', e.target.value)}
-                      className="education-input"
-                    />
-                    <label className="education-label">Description:</label>
-                    <input
-                      type="text"
-                      value={edu.description}
-                      onChange={(e) => handleEducationFieldChange(index, 'description', e.target.value)}
-                      className="education-input"
-                    />
-                    <button type="button" onClick={() => handleRemoveEducation(index)} className="education-remove-button">
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                  </div>
-                ))}
-                <button type="button" onClick={handleAddEducation}>
-                  Add Education
+        </div>
+        <div className="block">
+          <h3>Education</h3>
+          {education.map((edu, index) => (
+            <div className="card" key={index}>
+              <div className="card-header">
+                <button type="button" className="remove-button" onClick={() => handleRemoveEducation(index)}>
+                  <FontAwesomeIcon icon={faTrash} className="remove-icon" />
                 </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Work Experience:</td>
-              <td>
-                {workExperience.map((experience, index) => (
-                  <div className="work-experience-container" key={index}>
-                    <label className="work-experience-label">Company Name:</label>
-                    <input
-                      type="text"
-                      value={experience.companyName}
-                      onChange={(e) => handleWorkExperienceFieldChange(index, 'companyName', e.target.value)}
-                      className="work-experience-input"
-                    />
-                    <label className="work-experience-label">Year:</label>
-                    <input
-                      type="text"
-                      value={experience.year}
-                      onChange={(e) => handleWorkExperienceFieldChange(index, 'year', e.target.value)}
-                      className="work-experience-input"
-                    />
-                    <label className="work-experience-label">Description:</label>
-                    <input
-                      type="text"
-                      value={experience.description}
-                      onChange={(e) => handleWorkExperienceFieldChange(index, 'description', e.target.value)}
-                      className="work-experience-input"
-                    />
-                    <button type="button" onClick={() => handleRemoveWorkExperience(index)} className="work-experience-remove-button">
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                  </div>
-                ))}
-                <button type="button" onClick={handleAddWorkExperience}>
-                  Add Work Experience
+              </div>
+              <div className="card-body">
+                <div className="form-group">
+                  <label>School Name:</label>
+                  <input
+                    type="text"
+                    value={edu.schoolName}
+                    onChange={(e) => handleEducationFieldChange(index, 'schoolName', e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Year:</label>
+                  <input
+                    type="text"
+                    value={edu.year}
+                    onChange={(e) => handleEducationFieldChange(index, 'year', e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Description:</label>
+                  <input
+                    type="text"
+                    value={edu.description}
+                    onChange={(e) => handleEducationFieldChange(index, 'description', e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+          <button type="button" onClick={handleAddEducation}>
+            Add Education
+          </button>
+        </div>
+        <div className="block">
+          <h3>Work Experience</h3>
+          {workExperience.map((experience, index) => (
+            <div className="card" key={index}>
+              <div className="card-header">
+                <button type="button" className="remove-button" onClick={() => handleRemoveWorkExperience(index)}>
+                  <FontAwesomeIcon icon={faTrash} className="remove-icon" />
                 </button>
-              </td>
-            </tr>
-            <tr>
-              <td>Achievements:</td>
-              <td>
-                {achievements.map((achievement, index) => (
-                  <div className="achievement-container" key={index}>
-                    <label className="achievement-label">Type:</label>
-                    <input
-                      type="text"
-                      value={achievement.type}
-                      onChange={(e) => handleAchievementFieldChange(index, 'type', e.target.value)}
-                      className="achievement-input"
-                    />
-                    <label className="achievement-label">Description:</label>
-                    <input
-                      type="text"
-                      value={achievement.description}
-                      onChange={(e) => handleAchievementFieldChange(index, 'description', e.target.value)}
-                      className="achievement-input"
-                    />
-                    <button type="button" onClick={() => handleRemoveAchievement(index)} className="achievement-remove-button">
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                  </div>
-                ))}
-                <button type="button" onClick={handleAddAchievement}>
-                  Add Achievement
+              </div>
+              <div className="card-body">
+                <div className="form-group">
+                  <label>Company Name:</label>
+                  <input
+                    type="text"
+                    value={experience.companyName}
+                    onChange={(e) => handleWorkExperienceFieldChange(index, 'companyName', e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Year:</label>
+                  <input
+                    type="text"
+                    value={experience.year}
+                    onChange={(e) => handleWorkExperienceFieldChange(index, 'year', e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Description:</label>
+                  <input
+                    type="text"
+                    value={experience.description}
+                    onChange={(e) => handleWorkExperienceFieldChange(index, 'description', e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+          <button type="button" onClick={handleAddWorkExperience}>
+            Add Work Experience
+          </button>
+        </div>
+        <div className="block">
+          <h3>Achievements</h3>
+          {achievements.map((achievement, index) => (
+            <div className="card" key={index}>
+              <div className="card-header">
+                <button type="button" className="remove-button" onClick={() => handleRemoveAchievement(index)}>
+                  <FontAwesomeIcon icon={faTrash} className="remove-icon" />
                 </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </div>
+              <div className="card-body">
+                <div className="form-group">
+                  <label>Type:</label>
+                  <input
+                    type="text"
+                    value={achievement.type}
+                    onChange={(e) => handleAchievementFieldChange(index, 'type', e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Description:</label>
+                  <input
+                    type="text"
+                    value={achievement.description}
+                    onChange={(e) => handleAchievementFieldChange(index, 'description', e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+          <button type="button" onClick={handleAddAchievement}>
+            Add Achievement
+          </button>
+        </div>
         <br />
         <button type="submit" className='generate-pdf-button'>Generate PDF</button>
       </form>
